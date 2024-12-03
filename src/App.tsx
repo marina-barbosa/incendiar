aa8130e3f63cb886ee1f26a372850d1bc25eda19
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-12-03 04:24:38 -0300

    feat: add router e header no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Router>
+      <div className="min-h-screen bg-zinc-950">
+        <Header />
+        <main className="pt-16">
+          <Routes>
+            {/* <Route path="/" element={<Hero />} />
+            <Route path="/products/:category" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/account" element={<Account />} /> */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
