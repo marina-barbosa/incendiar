fef0a6726505b98c4c775f8fb08daeca5a0bc0a5
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-12-03 11:58:06 -0300

    refactor: home

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import Hero from "../components/hero";
+import { ProductGrid } from "../components/productGrid";
+
+export default function Home() {
+  return (
+    <>
+    <Hero />
+    <ProductGrid />
+    </>
+  );
+}
